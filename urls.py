09163bb9fb912bb3def59eb37fa8d1826cd06908
Guillehm7 <148593006+Guillehm7@users.noreply.github.com>
2026-01-11 23:01:57 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('ecuador/platos/', views.lista_platos, name='lista_platos'),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # ... tus rutas anteriores ...
+    path('admin-gui/', views.interfaz_administrador, name='admin_gui'),
+]
